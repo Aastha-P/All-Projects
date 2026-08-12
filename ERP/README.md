d6167f0e2a3e9822a69e26cbe22ffa66a1bd4161
Aastha Patel <114515584+Aastha-P@users.noreply.github.com>
2026-08-12 22:28:03 +0530

    Add files via upload

---
+++ ERP/README.md
@@ -0,0 +1,38 @@
+﻿# ERP Net Project
+
+**Enterprise resource planning (ERP)** is the integrated management of main business processes, often in real time and mediated bu software and technology.
+### This ERP Net
+Is created as a final project of the It Academy, which contains 4 main modules in the internal management of employees, CRM, sales, and inventory which contains: product, categories, movements, storage, warehouses made by clients.
+In each module the user can do differents CRUD operations
+It contains the employee users who can have the admin role to create other users, and to carry out special procedures. All users can login and make orders.
+
+### Used technology
+
+* In Backend DotNet Core with c# language
+* Entity Framework for Database persistance
+* LINQ
+* SQL Server Database
+* Automapper is an object object mapper. Works transforming an input object of one type into an output object of a different type
+* Repository Pattern to achive some objectives as: 
+	* Using Generic Repository which are bridges netween data and operations that are in different domains
+	* Increases the level of abstraction in code
+	* Isolate the data layer to support unit testing
+	* Access the data source from many locations and want to apply centrally managed, consistent access rules and logic
+	* Improve the code's maintainability and readability by separating business logic from data or service access logic and reducing the amount of redundant code
+
+* JWT For Authentication Login and Register
+* Frontend developed with Angular 2
+
+### NuGet Package Installed
+* AutoMapper
+* AutoMapper extensions for ASP.NET Core
+* Microsoft.AspNetCore.Authentication.JwtBearer
+* Microsoft.EntityFrameworkCore
+* Microsoft.EntityFrameworkCore.SqlServer
+* Microsoft.EntityFrameworkCore.Tool
+* System.IdentityModel.Token.Jwt
+
+If you'd like to quote someone, use the > character before the line:
+
+> Coffee. The finest organic suspension ever devised... I beat the Borg with it.
+> - Developed by Geraldine Hernández Al Attrach
---
+++ ERP/Startup.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using AutoMapper;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using ERPNet.Data;
+using ERPNet.Data.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using ERPNet.Helpers;
+using ERPNet.Services;
+
+namespace ERPNet
+{
+    public class Startup
+    {
+        public Startup ( IConfiguration configuration )
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices ( IServiceCollection services )
+        {
+           
+            // configure dbContext with SQL server db
+            services.AddDbContext<ERPNetContext> ( options =>
+                      options.UseSqlServer ( Configuration.GetConnectionString ( "ERPNetContext" ) ) );
+
+            services.AddCors ( options =>
+            {
+                options.AddPolicy ( "AllowSpecificOrigin",
+                    builder =>
+                    {
+                        builder.WithOrigins ( "http://localhost:4200" );
+                        builder.SetIsOriginAllowedToAllowWildcardSubdomains ()
+                        .AllowAnyHeader ()
+                        .AllowAnyMethod ()
+                        .AllowCredentials ();
+
+                    } );
+            } );
+
+            services.AddControllers ();
+
+            services.AddAutoMapper ( AppDomain.CurrentDomain.GetAssemblies () );
+
+            // configure strongly typed settings objects
+            var appSettingsSection = Configuration.GetSection ( "AppSettings" );
+            services.Configure<AppSettings> ( appSettingsSection );
+
+            // configure jwt authentication
+            var appSettings = appSettingsSection.Get<AppSettings> ();
+            var key = Encoding.ASCII.GetBytes ( appSettings.Secret );
+
+            // ADD Login JSON Web Token
+            services.AddAuthentication ( x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+               .AddJwtBearer ( options =>
+               {
+                   options.RequireHttpsMetadata = false;
+                   options.SaveToken = true;
+                   options.TokenValidationParameters = new TokenValidationParameters
+                   {
+                       ValidateIssuerSigningKey = true,
+                       IssuerSigningKey = new SymmetricSecurityKey ( key ),
+                       ValidateIssuer = false,
+                       ValidateAudience = false
+                   };
+               } );
+
+            // scope DI for application services
+            services.AddScoped<IUserService, UserService> ();
+            services.AddScoped<CustomerRepository> ();
+            services.AddScoped<EmployeeRepository> ();
+            services.AddScoped<AddressesRepository> ();
+            services.AddScoped<MovementsRepository> ();
+            services.AddScoped<OrderRepository> ();
+            services.AddScoped<OrderProductRepository> ();
+            services.AddScoped<ProductRepository> ();
+            services.AddScoped<StoragesRepository> ();
+            services.AddScoped<WarehouseRepository> ();
+    
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure ( IApplicationBuilder app, IWebHostEnvironment env )
+        {
+            if(env.IsDevelopment ())
+            {
+                app.UseDeveloperExceptionPage ();
+            }
+
+            app.UseHttpsRedirection ();
+
+            app.UseRouting ();
+
+            app.UseCors ( "AllowSpecificOrigin" );
+
+            app.UseAuthentication ();
+
+            app.UseAuthorization ();
+
+            app.UseEndpoints ( endpoints =>
+              {
+                  endpoints.MapControllers ();
+              } );
+        }
+    }
+}
